7b5fb2f51c93e47e895978c0f646d5682d977a32
StilaYN <dist.avdeev2004@gmail.com>
2023-11-21 09:53:40 +0700

    Для представление информации о цвете в конкретной точки изображения

---
+++ PhotoEditor/Pixel.cs
@@ -0,0 +1,17 @@
+﻿namespace PhotoEditor;
+
+public class Pixel
+{
+    public Pixel(byte red, byte green, byte blue, byte alpha)
+    {
+        Red = red;
+        Green = green;
+        Blue = blue;
+        Alpha = alpha;
+    }
+
+    public byte Red { get; set; }
+    public byte Green { get; set; }
+    public byte Blue { get; set; }
+    public byte Alpha { get; set; }
+}
