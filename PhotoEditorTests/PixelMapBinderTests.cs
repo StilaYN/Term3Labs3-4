53ea0a8ba928a520958438916631517c838fa9bb
StilaYN <dist.avdeev2004@gmail.com>
2023-11-22 13:00:11 +0700

    Для тестирования работы программы

---
+++ PhotoEditorTests/PixelMapBinderTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhotoEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEditor.Tests
+{
+    [TestClass()]
+    public class PixelMapBinderTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
