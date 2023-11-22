0a929db6ac4d91a7ed3374169a9ab9c5ee546cac
StilaYN <dist.avdeev2004@gmail.com>
2023-11-22 12:51:48 +0700

    Для загрузки изображения в виде массива байтов

---
+++ PhotoEditor/PixelMapLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace PhotoEditor;
+
+public class PixelMapLoader:IPixelMapLoader
+{
+    public (byte[], int,int) Load(string path)
+    {
+        var bitmap = new BitmapImage(new Uri(path));
+        var width = bitmap.PixelWidth;
+        var height = bitmap.PixelHeight;
+        int stride = width * ((bitmap.Format.BitsPerPixel + 7) / 8);
+        var buffer = new byte[stride * height];
+        bitmap.CopyPixels(buffer,stride,height);
+        return (buffer,width,height);
+    }   
+}   
