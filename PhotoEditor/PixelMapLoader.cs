e53772c295cf9947760f6521facb88475341550f
Nikita Feoktistov <nikitos.feo12@gmail.com>
2023-11-23 20:17:08 +0700

    Исправлена реализация PixelMapLoader.

--- PhotoEditor/PixelMapLoader.cs
+++ PhotoEditor/PixelMapLoader.cs
@@ -8,12 +8,12 @@ public class PixelMapLoader:IPixelMapLoader
 {
     public (byte[], int,int) Load(string path)
     {
-        var bitmap = new BitmapImage(new Uri(path));
-        var width = bitmap.PixelWidth;
-        var height = bitmap.PixelHeight;
-        int stride = width * ((bitmap.Format.BitsPerPixel + 7) / 8);
+        BitmapSource bitmap = new BitmapImage(new Uri(path));
+        int width = bitmap.PixelWidth;
+        int height = bitmap.PixelHeight;
+        int stride = (int)(width * ((bitmap.Format.BitsPerPixel) / 8));
         var buffer = new byte[stride * height];
-        bitmap.CopyPixels(buffer,stride,height);
+        bitmap.CopyPixels(buffer,stride,0);
         return (buffer,width,height);
     }   
 }   
