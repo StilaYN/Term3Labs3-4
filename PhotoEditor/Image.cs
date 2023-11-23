57feabe4b9e876982e44b7c99c8be2afa72ddbcc
StilaYN <dist.avdeev2004@gmail.com>
2023-11-23 18:09:41 +0700

    Для обозначения изображения в программе

---
+++ PhotoEditor/Image.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PhotoEditor;
+
+public class Image:IImage
+{
+    IPixel[,] _pixels;
+
+    public Image(IPixel[,] pixels)
+    {
+        this._pixels = pixels;
+    }
+
+    public IPixel[,] Pixels
+    {
+        get => _pixels;
+        set => _pixels = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int Width => _pixels.GetLength(1);
+    public int Height => _pixels.GetLength(0);
+}
