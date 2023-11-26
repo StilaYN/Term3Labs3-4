681afc753ac50103063eb69c7f4e20e42523c0a4
StilaYN <dist.avdeev2004@gmail.com>
2023-11-26 17:38:21 +0700

    Для перевода массива байтов в bitmap

---
+++ PhotoEditor/PixelMapUploader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace PhotoEditor;
+
+public class PixelMapUploader:IPixelMapUploader
+{
+    public BitmapSource Upload(byte[] date, int width, int height, int dpiX, int dpiY)
+    {
+        List<System.Windows.Media.Color> colors = new List<System.Windows.Media.Color>();
+        colors.Add(System.Windows.Media.Colors.Red);
+        colors.Add(System.Windows.Media.Colors.Blue);
+        colors.Add(System.Windows.Media.Colors.Green);
+        BitmapPalette myPalette = new BitmapPalette(colors);
+        int stride = (int)(width * ((PixelFormats.Bgr32.BitsPerPixel) / 8));
+        BitmapSource source = BitmapSource.Create(width,height,dpiX,dpiY,PixelFormats.Bgr32,myPalette,date,stride);
+        return source;
+    }
+}
