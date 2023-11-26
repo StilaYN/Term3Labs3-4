5ae54fa03ae818d7245ab234a818def57bd32a49
StilaYN <dist.avdeev2004@gmail.com>
2023-11-26 17:37:52 +0700

    Для сохранения изображений в формате jpg

---
+++ PhotoEditor/PixelMapSaverToJpg.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace PhotoEditor;
+
+public class PixelMapSaverToJpg:IPixelMapSaveToFile
+{
+    public void Save(BitmapSource image, string path)
+    {
+        using (FileStream fstream = new FileStream(path, FileMode.Create))
+        {
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.FlipHorizontal = true;
+            encoder.FlipVertical = false;
+            encoder.QualityLevel = 30;
+            encoder.Rotation = Rotation.Rotate90;
+            encoder.Frames.Add(BitmapFrame.Create(image));
+            encoder.Save(fstream);
+        }
+    }
+}
