ebffa3a0b2b40d1b3f151aa9bf57c81c18b81ecd
StilaYN <dist.avdeev2004@gmail.com>
2023-11-26 17:40:01 +0700

    Для сохранения изображения в файлы

---
+++ PhotoEditor/ImageSaver.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace PhotoEditor;
+
+public class ImageSaver:IImageSaver
+{
+    public IPixelMapSeparator Separator { get; set; }
+    public IPixelMapUploader Uploader { get; set; }
+    public IPixelMapSaveToFile Saver { get; set; }
+    public ImageSaver(IPixelMapSeparator separator, IPixelMapUploader uploader, IPixelMapSaveToFile saver)
+    {
+        this.Separator = separator;
+        this.Uploader = uploader;
+        this.Saver = saver;
+    }
+
+    public void Save(IImage image, int dpiX, int dpiY, string path)
+    {
+        (byte[] date, int width, int height, int bytePerPixel) = Separator.Separate(image.Pixels);
+        BitmapSource source =Uploader.Upload(date, width, height, dpiX, dpiY);
+        Saver.Save(source,path);
+    }
+}
