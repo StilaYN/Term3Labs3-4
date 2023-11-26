433afd4030d778ce8dc5da5261ffc87edd060dac
StilaYN <dist.avdeev2004@gmail.com>
2023-11-26 18:40:57 +0700

    Для удобства работы

---
+++ PhotoEditor/ConvertImageToBitmapSource.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace PhotoEditor;
+
+public class ConvertImageToBitmapSource:IConvertImageToBitmapSource
+{
+    public IPixelMapSeparator Separator { get; set; } 
+    public IPixelMapUploader Uploader { get; set; }
+    public ConvertImageToBitmapSource(IPixelMapSeparator separator, IPixelMapUploader uploader)
+    {
+        Separator = separator;
+        Uploader = uploader;
+    }
+
+    public BitmapSource ToBitmapSource(IImage image, int dpiX, int dpiY)
+    {
+        (byte[] data, int width, int height, int bytePerPixel) = Separator.Separate(image.Pixels);
+        BitmapSource source = Uploader.Upload(data, width, height, dpiX, dpiY);
+        return source;
+    }
+}
